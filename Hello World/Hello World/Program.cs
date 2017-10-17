bda19238b5b3a05a6396bbd26992f48878de6ce0
Horea Oros <horea.oros@gmail.com>
2017-10-17 12:02:37 +0300

    add arithmetic operators

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -11,6 +11,51 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Random rnd = new Random();
+
+            int r1 = 0;
+
+            // Console.WriteLine(rnd.Next(42));
+
+            
+
+
+            Console.WriteLine("Introduceti un numar: ");
+
+            string line;
+
+            line = Console.ReadLine();
+
+
+
+            try
+            {
+                r1 = int.Parse(line);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            
+
+
+
+            int r2;
+            r2 = rnd.Next(1, 7);
+            Console.WriteLine("r1 = {0}, r2 = {1}", r1, r2);
+
+            Console.WriteLine("r1 + r2 = {0}", r1 + r2);
+            Console.WriteLine("r1 - r2 = {0}", r1 - r2);
+            Console.WriteLine("r1 * r2 = {0}", r1 * r2);
+            Console.WriteLine("r1 / r2 = {0}", r1 / r2);
+            Console.WriteLine("r1 % r2 = {0}", r1 % r2);
         }
     }
 }
