caae0e9d4349da2fbeedf0a3396ba5f762a137d3
Horea Oros <horea.oros@gmail.com>
2017-10-31 10:11:59 +0200

    Add 3n+1

---
+++ Hello World/Hello2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instructiunea if
+            int an = 2016;
+            if ((an % 4 == 0 && an % 100 != 0) || (an % 400 == 0))
+            {
+                Console.WriteLine("{0} este bisect", an);
+            }
+            else
+            {
+                Console.WriteLine("{0} nu este bisect", an);
+            }
+
+
+            // instructiunea while
+            int suma = 0;
+            int n = 258964;
+            int backup;
+            int c;
+            backup = n;
+            while (n > 0)
+            {
+                c = n % 10; // obtin ultima cifra a lui n
+                suma = suma + c;
+                n = n / 10; // elimin ultima cifra a lui n
+            }
+            Console.WriteLine("Suma cifrelor numarului {0} este {1}", backup, suma);
+
+
+            // instructiunea do ... while
+            n = 10;
+            do
+            {
+                Console.WriteLine(n);
+                n--;
+                Thread.Sleep(1000);
+            } while (n > 0);
+            Console.Beep(1000, 500);
+
+            // instructiunea for
+            suma = 0;
+            for (int i = 0; i < 10; i += 2)
+            {
+                suma += i; // <==> suma = suma + i; a[x+y*z[j]][j%2 + n << 5] += 1;
+            }
+
+            // instructiunea switch
+            int zi = 3;
+
+            switch (zi)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("MidWeek");
+                    break;
+                case 5:
+                    Console.WriteLine("Finally Friday!");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Weekend");
+                    break;
+                default:
+                    Console.WriteLine("<error/>");
+                    break;
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Hello World/TreiNPlus1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreiNPlus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+
+            
+            Console.WriteLine("Pentru a = {0} lungimea sirului este: {1}", a, Lungime(a));
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                Lungime(i);
+            }
+        }
+
+        private static int Lungime(int a)
+        {
+            int contor = 0;
+            Console.WriteLine("{0} - ", a);
+            while (a != 1)
+            {
+                contor++;
+                if (a % 2 == 0) // testam paritatea numarului
+                {
+                    a = a / 2;
+                }
+                else
+                {
+                    a = 3 * a + 1;
+                }
+
+                Console.Write("{0} ", a);
+            }
+            Console.WriteLine("\n");
+            return contor;
+        }
+    }
+}
