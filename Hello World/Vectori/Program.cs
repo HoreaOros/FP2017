2aebe9646729b8bf7f7b3d8eb85e346770fcd676
Horea Oros <horea.oros@gmail.com>
2017-11-14 12:02:09 +0200

    add project Vectori.

---
+++ Hello World/Vectori/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v;
+            v = new int[10];
+            
+
+
+        }
+    }
+}
