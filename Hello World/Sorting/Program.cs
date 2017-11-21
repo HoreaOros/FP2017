76e4355b9dc454966adb1fb6b647579e3c1ce44c
Horea Oros <horea.oros@gmail.com>
2017-11-21 17:20:03 +0200

    add insertion sort

--- Hello World/Sorting/Program.cs
+++ Hello World/Sorting/Program.cs
@@ -19,12 +19,35 @@ namespace Sorting
                 v[i] = rnd.Next(100);
             }
 
-
+            // Selection Sort
+            Console.WriteLine("Selection Sort:");
             Shuffle.Program.DisplayArray(v);
-
             SelectionSort(v);
-
             Shuffle.Program.DisplayArray(v);
+
+
+            // Insertion Sort
+            Console.WriteLine("Insertion Sort:");
+            Shuffle.Program.ShuffleDeck(v);
+            Shuffle.Program.DisplayArray(v);
+            InsertionSort(v);
+            Shuffle.Program.DisplayArray(v);
+
+        }
+
+        private static void InsertionSort(int[] v)
+        {
+            
+            int aux;
+            for (int i = 1; i < v.Length; i++)
+            {
+                for (int k = i; k > 0 && v[k] < v[k - 1]; k--)
+                {
+                    aux = v[k];
+                    v[k] = v[k - 1];
+                    v[k - 1] = aux;
+                }
+            }
         }
 
         private static void SelectionSort(int[] v)
